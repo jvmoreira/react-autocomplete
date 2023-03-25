4a51289e90f5a182f8647fc31289bc6e861eb961
João Vitor Moreira <themoreira.jvm@gmail.com>
2023-03-25 09:57:11 -0300

    style: introduce App component with base style

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+import './style.scss';
+
+export function App(): ReactElement {
+
+  return (
+    <h1>Autocomplete Component</h1>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { App } from './app';
 
 const rootElement = document.getElementById('root');
 
@@ -6,4 +8,4 @@ if (!rootElement) {
   throw new Error('Missing root element');
 }
 
-createRoot(rootElement).render('Hello World');
+createRoot(rootElement).render(<App />);
