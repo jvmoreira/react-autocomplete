1122d3df619478fc5c01fcd7234bcf8c6db158f5
João Vitor Moreira <themoreira.jvm@gmail.com>
2023-03-26 14:54:22 -0300

    feat: render Autocomplete Field inside App component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import React, { ReactElement } from 'react';
+import { getNpmPackageNameAutocompleteOptions } from './lib/autocomplete-fetcher/npm-package-name-autocomplete-fetcher';
+import { AutocompleteField } from './autocomplete-field';
 import './style.scss';
 
 export function App(): ReactElement {
@@ -6,6 +8,8 @@ export function App(): ReactElement {
   return (
     <main className="page-content">
       <h1 className="page-title">Autocomplete Component</h1>
+
+      <AutocompleteField placeholder="Search NPM packages" fetcher={getNpmPackageNameAutocompleteOptions} />
     </main>
   );
 }
